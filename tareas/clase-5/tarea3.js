7beae48faa651ac279f57424734a623b560f6c9e
Lean-Lopez <leandrolopezsanm@gmail.com>
2022-01-13 12:21:41 -0300

    Calcula horas totales ingresadas
    El usuario ingresa las horas de cada clase y las sumo para poder procesarlas.

--- tareas/clase-5/tarea3.js
+++ tareas/clase-5/tarea3.js
@@ -22,3 +22,10 @@ const $calcularTiempoTotal = document.querySelector("#calcular-tiempo-total");
 
 $calcularTiempoTotal.onclick = function() {
     
+    const horasClase1 = Number(document.querySelector("#horas-clase-1").value);
+    const horasClase2 = Number(document.querySelector("#horas-clase-2").value);
+    const horasClase3 = Number(document.querySelector("#horas-clase-3").value);
+    const horasClase4 = Number(document.querySelector("#horas-clase-4").value);
+    const horasClase5 = Number(document.querySelector("#horas-clase-5").value);
+
+    let horasTotales = horasClase1 + horasClase2 + horasClase3 + horasClase4+ horasClase5;
